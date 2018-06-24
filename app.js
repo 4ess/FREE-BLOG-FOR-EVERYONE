b9883ef1ec4d218ceec2db3181ed9f4bdc9ca17a
Dionis Dimcea <31817527+4ess@users.noreply.github.com>
2018-06-24 15:47:49 +0300

    v.0.3.9
    +frontend
    ++editor
    ++add PubLogs
    +backend
    ++routes

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
@@ -14,6 +15,8 @@ require('./config/passport')(passport);
 //Load Routes
 const index = require('./routes/index');
 const auth = require('./routes/auth');
+const stories = require('./routes/stories');
+
 
 //Load Keys
 const keys = require('./config/keys');
@@ -49,10 +52,14 @@ app.use((req, res, next) => {
     next();
 });
 
+//Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 //Use Routes
 app.use('/', index);
-
 app.use('/auth', auth);
+app.use('/stories', stories);
+
 
 const port = process.env.PORT || 5000;
 
